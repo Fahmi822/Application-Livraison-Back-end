471ffe8683c7d2fab7ee0a237cf454ce2b5a326f
sadraouizeineb <sadraouizeineb@gmail.com>
2025-03-13 21:31:11 +0100

    fixes

--- Program.cs
+++ Program.cs
@@ -1,14 +1,19 @@
 using Microsoft.EntityFrameworkCore;
 using Application_Livraison_Backend.Data;
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; 
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Ajouter la chaîne de connexion de MySQL à partir de appsettings.json
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseMySQL(
-        builder.Configuration.GetConnectionString("DefaultConnection") // Connexion à la base de données MySQL
+    options.UseMySql(
+        builder.Configuration.GetConnectionString("DefaultConnection"),
+        new MySqlServerVersion(new Version(8, 0, 30)) // Assurez-vous d'utiliser la bonne version
     )
 );
+
 
 // Ajouter les services Razor Pages
 builder.Services.AddRazorPages();
